a42ea23fa85e5b1ca430dd903176386cf1557a42
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2025-11-28 21:11:11 +0300

    Update customer offer page styling and content
    Enhanced the appearance of the offer card with padding, font weight, and underline. Adjusted text size for the offer description. Replaced placeholder text with a styled heading for 'EspressoYourself'.

--- client/src/components/customer/page.tsx
+++ client/src/components/customer/page.tsx
@@ -2,18 +2,22 @@ export default function Customer() {
   return (
     <div className="py-20 mx-auto bg-amber-100 max-w-6xl">
       <div className=" grid mx-auto px-6 md:grid-cols-2 gap-12 items-center ">
-        <div className="h-[240px] bg-white  rounded-md justify items-center text-center p-2 shadow-md">
+        <div className="h-[240px] bg-white  rounded-md justify items-center text-center p-3 shadow-md">
           {" "}
-          <h1 className="text-[30px] text-amber-700 semi-bold ">
+          <h1 className="text-[30px] text-amber-700 font-bold underline p-2 ">
             100th customer offer
           </h1>
-          <p className="text-[20px] text-gray-700 mt-4 text-justify leading-relaxed">
+          <p className="text-[17px] text-gray-700 mt-4 text-justify ">
             Get a free coffee on us! Be our 100th customer and enjoy a
             complimentary beverage of your choice as a token of our
             appreciation.
           </p>
         </div>
-        <div>harry</div>
+        <div>
+          <h1 className="text-center text-amber-900 font-bold text-[40px]">
+            EspressoYourself
+          </h1>
+        </div>
       </div>
     </div>
   );

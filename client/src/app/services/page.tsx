9bc4954c0c2e58ec2aa601ca191f6be370938a0d
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2025-11-28 22:24:56 +0300

    Update Services page with new layout and content
    Replaces placeholder text with a styled section introducing 'Our Services' and a descriptive subtitle. Adds Tailwind CSS classes for improved layout and appearance.

--- client/src/app/services/page.tsx
+++ client/src/app/services/page.tsx
@@ -1,7 +1,14 @@
 export default function Services() {
   return (
-    <div>
-      <h1>hello harry</h1>
+    <div className="mx-auto bg-slate">
+      <div className="text-center px-6 py-6 bg-amber-100">
+        <h1 className="text-3xl font-semibold text-amber-900 mb-3">
+          Our Services{" "}
+        </h1>
+        <p className="text-black text-xl ">
+          The best that can ever exist in a coffee shop{" "}
+        </p>
+      </div>
     </div>
   );
 }
